0d8fc00bd6b5201e45f02268ddfd38392f1b27ff
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-10 10:27:20 +0100

    fine esercizio

---
+++ script.js
@@ -0,0 +1,17 @@
+const items = [
+    'Latte',
+    'Uova',
+    'Pane',
+    'Acqua',
+    'Fazzoletti',
+
+];
+const itemsList = document.getElementById('items');
+let i = 0
+while (i < items.length) {
+    const newLi = document.createElement('li');
+    newLi.innerHTML = items[i];
+    itemsList.append(newLi);
+
+    i++;
+}
